94cb545e77331083587a225eb26178ed4059dfa3
Shougata <shougatapaul46@gmail.com>
2024-04-04 23:45:08 +0600

    Thailand & Indonesia listing card done

--- src/components/listingCard/ListingCard.jsx
+++ src/components/listingCard/ListingCard.jsx
@@ -12,6 +12,12 @@ import { Pagination } from "swiper/modules";
 import image1 from "../../assets/listings/paris-1.webp";
 import image2 from "../../assets/listings/paris-2.webp";
 import image3 from "../../assets/listings/paris-3.webp";
+import image4 from "../../assets/listings/thai-1.webp";
+import image5 from "../../assets/listings/thai-2.webp";
+import image6 from "../../assets/listings/thai-3.webp";
+import image7 from "../../assets/listings/indo-1.webp";
+import image8 from "../../assets/listings/indo-2.webp";
+import image9 from "../../assets/listings/indo-3.webp";
 
 const ListingCard = () => {
   return (
@@ -37,26 +43,6 @@ const ListingCard = () => {
       <p>Dedicated Worksapce</p>
       <p>$1800 month</p>
 
-      <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src={image1} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={image2} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={image3} />
-        </SwiperSlide>
-      </Swiper>
-      <h2>Condo in Paris</h2>
-      <p>Dedicated Worksapce</p>
-      <p>$1800 month</p>
 
       <Swiper
         pagination={{
@@ -66,18 +52,39 @@ const ListingCard = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img src={image1} />
+          <img src={image4} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={image2} />
+          <img src={image5} />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={image3} />
+          <img src={image6} />
         </SwiperSlide>
       </Swiper>
-      <h2>Condo in Paris</h2>
-      <p>Dedicated Worksapce</p>
-      <p>$1800 month</p>
+      <h2>Chang Mai, Thailand</h2>
+      <p>Entire villa with a pool </p>
+      <p>$1400 month</p>
+
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={image7} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image8} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image9} />
+        </SwiperSlide>
+      </Swiper>
+      <h2>Treehouse in Klungkung, Indonesia</h2>
+      <p>Cliffside Bamboo Treehouse - Private Pool & View</p>
+      <p>$2000 month</p>
 
       <Swiper
         pagination={{
